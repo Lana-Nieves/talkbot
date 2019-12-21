6d8fd1fe0ee164a3e6c5be7b21c9c09caf8f990f
aaron <aaron@aaron-m.co.nz>
2019-12-21 18:53:50 +1300

    working

--- src/sql/00001_create-guild-table.sql
+++ src/sql/00001_create-guild-table.sql
@@ -1,11 +1,12 @@
-create table guild (
-  guild_id integer not null,
-  name text not null,
-  default_voice_provider_id integer null,
-  command_prefix text default('!'),
-  title text not null default('master'),
-  default_language text default('en-US'),
-  updated timestamp with time zone not null,
-  created timestamp with time zone not null,
-  constraint PK_guild primary key(guild_id),
-);
+CREATE TABLE guild
+(
+    guild_id integer NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    default_voice_provider text COLLATE pg_catalog."default" DEFAULT 'amazon'::text,
+    command_prefix text COLLATE pg_catalog."default" DEFAULT '!'::text,
+    title text COLLATE pg_catalog."default" NOT NULL DEFAULT 'master'::text,
+    default_language text COLLATE pg_catalog."default" DEFAULT 'en-US'::text,
+    updated timestamp with time zone NOT NULL,
+    created timestamp with time zone NOT NULL,
+    CONSTRAINT "PK_guild" PRIMARY KEY (guild_id)
+)
